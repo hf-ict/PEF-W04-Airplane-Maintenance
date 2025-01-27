1d33d81e06f67575aa74e7f611b0b8f1b639853b
Roger Zaugg <roger.zaugg@sbl.ch>
2025-01-27 11:25:41 +0100

    added Airplane and prepared classes to implement

---
+++ src/Airplane.java
@@ -0,0 +1,47 @@
+
+public class Airplane {
+
+    private String airplaneType;
+    private int currentPassengerCount;
+    private int currentAltitude; // in feet
+    private double currentSpeed; // in knots
+
+    public Airplane(String airplaneType) {
+        this.airplaneType = airplaneType;
+    }
+
+    public void setAirplaneType(String airplaneType) {
+        this.airplaneType = airplaneType;
+    }
+
+    public int getCurrentPassengerCount() {
+        return currentPassengerCount;
+    }
+
+    public int getCurrentAltitude() {
+        return currentAltitude;
+    }
+
+    public double getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public void start(double targetSpeed) {
+        this.currentSpeed = targetSpeed;
+    }
+
+    public void land() {
+        this.currentAltitude = 0;
+        this.currentSpeed = 0;
+    }
+
+    public void flight(int newAltitude, double newSpeed) {
+        this.currentAltitude = newAltitude;
+        this.currentSpeed = newSpeed;
+    }
+
+    public int boarding(int passengers) {
+        this.currentPassengerCount += passengers;
+        return this.currentPassengerCount;
+    }
+}
---
+++ src/MaintenanceApp.java
@@ -0,0 +1,9 @@
+public class MaintenanceApp {
+    public static void main(String[] args) {
+
+        Airplane boeing = new Airplane("Boeing 747");
+        Airplane airbus = new Airplane("Airbus A320");
+
+        //TODO: add hangar handling
+    }
+}
